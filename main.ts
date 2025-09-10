d2103e223baa324a3d22ebac25b3f5b8d73f40aa
Didi <mlambis2010@icloud.com>
2025-09-10 15:10:18 +0930

    Radio changes

--- main.ts
+++ main.ts
@@ -11,19 +11,20 @@ input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    ny = 0
-    nx = 0
+    radio2 += 1
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
 })
+let radio2 = 0
 let nx = 0
 let ny = 0
 radio.setGroup(1)
+basic.clearScreen()
 ny = 0
 nx = 0
 basic.forever(function () {
+    radio.setGroup(radio2)
     if (nx == 5) {
         nx = 0
         ny += 1
